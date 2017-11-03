98e438f4895e70c6ddcf656072f5e5a035de2484
CANDELASAMD <candelasamd@madrid.es>
2017-11-03 11:48:17 +0100

    commit final clase

--- blog/app/controllers/pages_controller.rb
+++ blog/app/controllers/pages_controller.rb
@@ -1,5 +1,46 @@
 class PagesController < ApplicationController
+
+    before_action :set_page, only: [:show, :edit, :update, :destroy]
+
     def index
         @pages=Page.all
     end
+
+    def show
+        #render html: params[:id]
+    end
+
+    def new
+        @page=Page.new
+    end
+    def create
+        @page=Page.new(page_params())
+        @page.save
+        #render plain: params.to_json
+        redirect_to @page
+    end
+
+    def edit
+    end
+
+    def update
+        @page.update(page_params())
+        redirect_to @page
+    end
+
+    def destroy
+        @page.destroy
+        redirect_to pages_path
+        #redirect_to pages_url
+    end
+
+    private 
+    def set_page
+        @page=Page.find(params[:id])
+    end
+    def page_params
+        params.require(:page).permit(:title,:body,:slug)
+    end
 end
+
+
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :posts
-
-  get '/pages', to: 'pages#index'
+  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #post '/pages', to: 'pages#create'
+
+  #get '/pages', to: 'pages#index'
+  #get '/pages/new', to: 'pages#new', as: 'new_page'
+  #get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  #get '/pages/:id', to: 'pages#show', as: 'page'
+
+  #patch '/pages/:id', to: 'pages#update'
+  #delete '/pages/:id', to: 'pages#destroy'
 end
